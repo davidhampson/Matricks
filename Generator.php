5c29ad76e9e01df80a6202c3b56e47e599198a92
David J. D. Hampson <davidhampson.97@gmail.com>
2017-04-17 07:45:07 -0700

    Added passwords and input gaurds
    Added passwords and input gaurds

--- Generator.php
+++ Generator.php
@@ -10,31 +10,34 @@ include("MatrixException.php");
 include("LUDecomposition.php");
 use MCordingley\LinearAlgebra\Matrix;
 
+
 // Grade student quiz if submitted
 if (isset($_POST["STUDENT"]) && isset($_POST["SUBMITTED"])){
 	SaveMark(RetrieveMatrices($_POST["msize"], $_POST["displayamount"]));
+	backToQgen();
 
 // Show class results if there is an id on the url
-} elseif (strpos($_SERVER["REQUEST_URI"], "?id=") !== false) {
+} elseif ($_POST["CHECKMARK"]) {
 	echo "<h1>Class Results:</h1>";
-	$request = $_SERVER["REQUEST_URI"];
-	$uniqueid = substr($_SERVER["REQUEST_URI"], strpos($_SERVER["REQUEST_URI"], "?id=")+4, strlen($request));
-	ShowMarks($uniqueid);
-// 
+	ShowMarks($_POST["KEY"], $_POST["PASS"]);
 } else {
-	if (!empty($_GET["STUDENT"])) {
+	if (!empty($_GET["STUDENT"])) { // Show quiz for id entered
 		ShowQuizStudent($_GET["STUDENT"], $_GET["classid"], $_GET["firstname"], $_GET["lastname"], $_GET["studentid"]);
-	} elseif (!empty($_POST["TEACHER"])){
-		ShowQuizTeacher($_POST["name"], $_POST["class"], $_POST["TEACHER"], $_POST["operations"], $_POST["msize"], $_POST["range1"], $_POST["range2"], $_POST["displayamount"]);
+	} elseif (!empty($_POST["TEACHER"])){ // Shows the teacher the quiz he created
+		ShowQuizTeacher($_POST["name"], $_POST["class"], $_POST["TEACHER"], $_POST["operations"], $_POST["msize"], $_POST["range1"], $_POST["range2"], $_POST["displayamount"], $_POST["password"]);
 	} else {
-		GenerateQuiz($_POST["GUEST"], $_POST["operations"], $_POST["msize"], $_POST["range1"], $_POST["range2"], $_POST["displayamount"]);
+		if (isset($_POST["PDF"])) { // PDF mode
+			GenerateQuiz($_POST["GUEST"], $_POST["operations"], $_POST["msize"], $_POST["range1"], $_POST["range2"], $_POST["displayamount"], "PDF");
+		} else { // Guest mode
+			GenerateQuiz($_POST["GUEST"], $_POST["operations"], $_POST["msize"], $_POST["range1"], $_POST["range2"], $_POST["displayamount"]);
+		}
 	}
 }
 
 ///////////////////////////////////////////////////////////////////HIGHEST LEVEL DISPLAY FUCNTIONS
 
 // Shows the quize to a user wanting to practice
-function GenerateQuiz($identity, $operation, $size, $r1, $r2, $displayamount) {
+function GenerateQuiz($identity, $operation, $size, $r1, $r2, $displayamount, $mode="Input") {
 	$uniqueid = uniqid();
 	
 	if ($r1 > $r2) {
@@ -60,13 +63,13 @@ function GenerateQuiz($identity, $operation, $size, $r1, $r2, $displayamount) {
 	echo "<input type=\"hidden\" name=\"range1\" value=\"$lowerBound\"/>";
 	echo "<input type=\"hidden\" name=\"GUEST\" value=\"$identity\"/>";
 
-	ShowQuiz($matrixarray, $operation);
+	ShowQuiz($matrixarray, $operation, $mode);
 
 	echo "</form>";
 }
 
 // Shows the quiz to a teacher that just made the quiz
-function ShowQuizTeacher($teachname, $classname, $identity, $operation, $size, $r1, $r2, $displayamount) {
+function ShowQuizTeacher($teachname, $classname, $identity, $operation, $size, $r1, $r2, $displayamount, $password) {
 	if ($r1 > $r2) {
 		$upperBound = $r1;
 		$lowerBound = $r2;
@@ -76,14 +79,14 @@ function ShowQuizTeacher($teachname, $classname, $identity, $operation, $size, $
 	}
 
 	$matrixarray = GenerateMatrices($size, $displayamount, $lowerBound, $upperBound, $operation);
-	$uniqueid = SaveQuiz($teachname, $classname, $identity, $matrixarray, $operation, $size, $displayamount);
+	$uniqueid = SaveQuiz($teachname, $classname, $identity, $matrixarray, $operation, $size, $displayamount, $password);
 	
-	echo "To access the student's results, you can go <a href=\"http://kymotsujason.ca/qgen/Generator.php?id=$uniqueid\">here</a>.";
-	echo "<br>";
+	echo "<h1>ID: $uniqueid</h1><br>";
 	echo "Students can take the test by going to the homepage (kymotsujason.ca/qgen), <br>
-	pressing the 'students' button and entering the following 'class id' or unique id: $uniqueid";
+	pressing the 'student' button and entering the ID. You can check the grades by presing <br>
+	'teacher', then 'check marks'. Don't forget the password you set.";
+	
 	echo "<br><h1>Answer Key</h1><br>";
-
 	ShowQuiz($matrixarray, $operation, "Show Answer");
 }
 
@@ -98,6 +101,11 @@ function ShowQuizStudent($identity, $uniqueid, $firstname, $lastname, $studentid
 			$teachname = $row["prof"];
 			$classname = $row["class"];
 			$element = $row["elements"];
+		}
+		else {
+			echo "Invalid Quiz ID.";
+			backToQgen();
+			return;
 		}
 	} else {
 		echo "Problem with SQL. <br>" . $link->error;
@@ -128,7 +136,7 @@ function ShowQuizStudent($identity, $uniqueid, $firstname, $lastname, $studentid
 	echo "<input type=\"hidden\" name=\"studentid\" value=\"$studentid\"/>";
 	echo "<input type=\"hidden\" name=\"operations\" value=\"$operation\"/>";
 
-	ShowQuiz($matrixarray, $matrixarray["operation"]);
+	ShowQuiz($matrixarray, $matrixarray["operation"],"Quiz");
 
 	echo "</form>";
 }
@@ -138,7 +146,12 @@ function ShowQuizStudent($identity, $uniqueid, $firstname, $lastname, $studentid
 // "Input"		 			Allows user to input answers
 // "Show Answer" 			Shows correct answer
 // "Show Student Answer"	Shows student answer, pulled from the matrix array
+// "PDF"					Shows only the question
 function ShowQuiz($matrixarray, $operation, $mode="Input") {
+
+	if ($mode == "Input") {
+		echo "<form><input type=\"submit\" value=\"PDF mode\" class=\"PDFButton\" name=\"PDF\"></form>";
+	}
 
 	$len = count($matrixarray);
 
@@ -183,21 +196,29 @@ function ShowQuiz($matrixarray, $operation, $mode="Input") {
 			DisplayMatrix($matrixarray[$i][2]);
 		} elseif ($mode === "Show Student Answer") {
 			DisplayMatrix($matrixarray[$i][3]);
-		} else {
+		} elseif ($mode === "Input" || $mode === "Quiz") {
 			DisplayInputs($matrixarray[$i], $i);
+		} elseif ($mode === "PDF") {
+			DisplayEmptyMatrix($matrixarray[0][0]->getColumnCount());
 		}
 		echo "</td>";
 		echo "</tr>";
 	}
 
 	echo "<tr><td></td><td></td><td></td><td></td><td></td><td>";
-	if ($mode === "Input") {
+	if ($mode === "Input" || $mode === "Quiz") {
 		echo "<input type=\"submit\" value=\"Submit\" class=\"submitButton\" name=\"SUBMITTED\">";
 	}
 	echo "</td></tr></table>";
 }
 
 ///////////////////////////////////////////////////////////////////LOWEST LEVEL DISPLAY FUNCTIONS
+
+// displays a button that takes you back to the main page
+
+function backToQgen() {
+	echo "<form action=\"http://kymotsujason.ca/qgen/index.html\"><input type=\"submit\" class=\"backToQgen\" value=\"Back to QGen\" /></form>";
+}
 
 // displays a matrix in html
 function DisplayMatrix($matrix) {
@@ -207,6 +228,19 @@ function DisplayMatrix($matrix) {
 		for ($j = 0; $j < $matrix->getRowCount(); $j++) {
 			$arrayValue = $matrix->get($i, $j);
 			echo "<td class=\"matrixData\"> $arrayValue</td>";
+		}
+		echo "</tr>";
+	}
+	echo "</table>";
+}
+
+// empyty matrix
+function DisplayEmptyMatrix($size) {
+	echo "<table class=\"matrixArray\"><tr>";
+	for ($i = 0; $i < $size; $i++) {
+		echo "<tr>";
+		for ($j = 0; $j < $size; $j++) {
+			echo "<td class=\"matrixData\">&nbsp;</td>";
 		}
 		echo "</tr>";
 	}
@@ -286,15 +320,15 @@ function SaveState($matrixarray, $operation, $displayamount, $depth=3) {
 }
 
 // Saves quiz to database
-function SaveQuiz($teachname, $classname, $identity, $matrixarray, $operation, $size, $displayamount) {
+function SaveQuiz($teachname, $classname, $identity, $matrixarray, $operation, $size, $displayamount, $password) {
 
 	// condense matrices and operation to an ASCII string
 	$elements = SaveState($matrixarray, $operation, $displayamount);
 	$uniqueid = uniqid();
 	$link = new mysqli("localhost", "kymot_qgen", "cmpt386isaclass", "kymotsujason_qgen");
 	if (!$link->connect_error) {
-		$sqlInsert = "INSERT INTO QUIZZES (id, class, prof, elements)
-		VALUES ('$uniqueid', '$classname', '$teachname', '$elements')";
+		$sqlInsert = "INSERT INTO QUIZZES (id, class, prof, elements, password)
+		VALUES ('$uniqueid', '$classname', '$teachname', '$elements', '$password')";
 		$link->query($sqlInsert);
 	} else {
 		echo "Problem with SQL. <br>" . $link->error;
@@ -325,29 +359,59 @@ function SaveMark($matrixarray) {
 	// display student's mark
 	$displayamount = $_POST["displayamount"];
 	$mark = "$correct" . "/" . "$displayamount";
-	echo "<h1>Your mark is: $mark</h1>";
 
 	$uniqueid = $_POST["id"];
 	$studentid = $_POST["studentid"];
 	$operation = $_POST["operation"];
+	
+	if ($studentid == "") {
+		echo "Invalid student id";
+		return;
+	}
+
 	$elements = SaveState($matrixarray, $operation, $displayamount, 4); // 4 is for depth, see SaveState for more info
 	// upload to database
 	$link = new mysqli("localhost", "kymot_qgen", "cmpt386isaclass", "kymotsujason_qgen");
 	if (!$link->connect_error) {
-		$sqlInsert = "INSERT INTO GRADES (quizNum, studentNum, grade, elements)
-		VALUES ('$uniqueid', '$studentid', '$mark', '$elements')";
-		$link->query($sqlInsert);
+		$sqlSelect = "SELECT * FROM GRADES WHERE quizNum = '$uniqueid' AND studentNum = '$studentid'";
+		$result = $link->query($sqlSelect);
+		if ($result->num_rows > 0) {
+				echo "$studentid has already completed this quiz.";
+				$link->close();
+				return;
+		} else {
+				$sqlInsert = "INSERT INTO GRADES (quizNum, studentNum, grade, elements) VALUES ('$uniqueid', '$studentid', '$mark', '$elements')";
+				$link->query($sqlInsert);
+		}
 	} else {
 		echo "Problem with SQL. <br>" . $link->error;
 	}
 	$link->close();
+
+	echo "<h1>Your mark is: $mark</h1>";
 }
 
 // Looks at database to see marks of students who have taken the given exam.
-function ShowMarks($uniqueid) {
+function ShowMarks($uniqueid, $password) {
+
 	// pull marks from database
 	$link = new mysqli("localhost", "kymot_qgen", "cmpt386isaclass", "kymotsujason_qgen");
+	// check password and id
+	$checkPW = "SELECT password FROM QUIZZES WHERE id='$uniqueid'";
+	$checkPass = $link->query($checkPW);
+	if ($checkPass->num_rows == 1) {
+		$quizRow = $checkPass->fetch_assoc();
+		if ($quizRow["password"] != $password) {
+			echo "Invalid password.";
+			return;		
+		} 
+	} else {
+		echo "Invalid ID.";
+		return;
+	}
 	
+
+
 	if (isset($_POST["showresult"])) {	
 		$studentId = $_POST["studentid"];
 		$matrixarray=ReconstructMatrices($_POST["elements"]);
@@ -365,11 +429,15 @@ function ShowMarks($uniqueid) {
 				$elements = $row["elements"];
 				$studentid = $row["studentNum"];
 				$operation = array_pop(explode("_", $elements));
+				echo "  ";
 				echo "<form method=\"POST\" id=\"showresult\">";
 				echo "<input type=\"hidden\" name=\"studentid\" value=\"$studentid\"/>";
 				echo "<input type=\"hidden\" name=\"elements\" value=\"$elements\"/>";
 				echo "<input type=\"hidden\" name=\"operation\" value=\"$operation\"/>";
-				echo "<input type=\"submit\" value=\"See Answer\" name=\"showresult\">";
+				echo "<input type=\"hidden\" name=\"PASS\" value=\"$password\" />";
+				echo "<input type=\"hidden\" name=\"KEY\" value=\"$uniqueid\" />";
+				echo "<input type=\"hidden\" name=\"CHECKMARK\" value=\"true\" />";
+				echo "<input type=\"submit\" value=\"See Answer\" name=\"showresult\"></form>";
 				echo "<br><br>";
 			}
 		} else {
